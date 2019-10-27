c4edecf9fdc1e530be42c6665e500a20161980bd
Kevin Deng <kevindeng121@gmail.com>
2019-10-26 23:17:17 -0400

    Rectangle can move right

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AceEditor from 'react-ace';
+import Konva from 'konva';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function onChange(newValue) {
+  console.log('change',newValue);
+}
+
+class MyRect extends React.Component {
+  moveForward = () => {
+    // to() is a method of `Konva.Node` instances
+    this.rect.to({
+      x: this.rect.getAttr('x') + 50,
+      duration: .5
+    });
+  };
+  render() {
+    return (
+      <Rect
+        ref={node => {
+          this.rect = node;
+        }}
+        width={50}
+        height={50}
+        fill="green"
+        draggable
+        onClick={this.moveForward}
+      />
+    );
+  }
+}
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>
+            Animata
+          </h1>
+        </header>
+        
+        <div class="col">
+          <AceEditor
+            theme="github"
+            onChange={onChange}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{$blockScrolling: true}}
+          />
+        </div>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <MyRect />
+          </Layer>
+        </Stage>
+      </div>
+    );
+  }
+
 }
 
 export default App;
