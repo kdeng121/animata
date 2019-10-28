e2a61333e169b1a017f31a05acc68cc8a8fbfd3c
Kevin Deng <kevindeng121@gmail.com>
2019-10-27 22:42:55 -0400

    Add circle and new canvas color

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Konva from 'konva';
 import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 import MyRect from './Components/MyRect'; // Import a component from another file
+import MyCircle from './Components/MyCircle'; // Import a component from another file
+
 
 
 function onChange(newValue) {
@@ -15,7 +17,7 @@ function onChange(newValue) {
 
 class App extends Component {
 
-  addObject(){
+  addRectangle(){
     const newRectangle = {
       x: 10,
       y: 10,
@@ -25,6 +27,17 @@ class App extends Component {
       id: 'rect3'
     };
     this.setState({ rects: [...this.state.rects, newRectangle] })
+  }
+
+  addCircle(){
+    console.log("CIRCLE ADDED BUT");
+    const newCircle = {
+      x: 200,
+      y: 200,
+      radius: 50,
+      fill: 'black',
+    };
+    this.setState({ circles: [...this.state.circles, newCircle] })
   }
 
   constructor(props){
@@ -47,6 +60,14 @@ class App extends Component {
           fill: 'green',
           id: 'rect2'
         }
+      ],
+      circles: [
+        {
+          x: 100,
+          y: 100,
+          radius: 50,
+          fill: 'black',
+        }
       ]
     }
   }
@@ -62,26 +83,37 @@ class App extends Component {
         </header>
         
         <div className="rowC">
-          <div>
+          <div className="editor">
             <AceEditor
               theme="monokai"
               onChange={onChange}
               name="UNIQUE_ID_OF_DIV"
               editorProps={{$blockScrolling: true}}
+              fontSize={16}
             />
-            <button onClick={this.addObject.bind(this)}>Add Rectangle!</button>
+            <button onClick={this.addRectangle.bind(this)}>Add Rectangle!</button>
+            <button onClick={this.addCircle.bind(this)}>Add Circle!</button>
+
 
           </div>
           
 
-          <div>
-            <Stage width={800} height={800}>
+          <div className="stage">
+            <Stage width={700} height={600}>
               <Layer>
                 {this.state.rects.map((rect, i) => {
                   return (
                     <MyRect
                       key={i}
                       shapeProps={rect}
+                    />
+                  );
+                })}
+                {this.state.circles.map((circ, i) => {
+                  return (
+                    <MyCircle
+                      key={i}
+                      shapeProps={circ}
                     />
                   );
                 })}
---
+++ src/Components/MyCircle.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+
+
+class MyCircle extends React.Component {
+    moveForward = () => {
+      // to() is a method of `Konva.Node` instances
+      this.circle.to({
+        x: this.circle.getAttr('x') + 50,
+        duration: .5
+      });
+    };
+    
+    render() {
+      return (
+        <Circle
+          ref={node => {
+            this.circle = node;
+          }}
+          x={this.props.shapeProps.x}
+          y={this.props.shapeProps.y}
+          radius={this.props.shapeProps.radius}
+          fill={this.props.shapeProps.fill}
+          id={this.props.shapeProps.id}
+          draggable
+          onClick={this.moveForward}
+        />
+        );
+    }
+}
+
+export default MyCircle;
+
