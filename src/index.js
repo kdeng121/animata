36284f899e0d1a6533fb134def148c3b2483c505
Kevin Deng <kevindeng121@gmail.com>
2019-12-09 20:19:17 -0500

    Build for deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { hydrate, render } from "react-dom";
+
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+//render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
